63a8a1366c8a44820c7d44d56005c89bf0a05fe1
Tim Smith <tim.smith.hdg@gmail.com>
2016-02-09 17:50:37 -0800

    first push: theme start, html css

---
+++ header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<?php // force Internet Explorer to use the latest rendering engine available ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<title><?php wp_title(''); ?></title>
+
+		<?php // mobile meta (hooray!) ?>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<?php // or, set /favicon.ico for IE10 win ?>
+		<meta name="msapplicatmake your own wordpress sliderion-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+            <meta name="theme-color" content="#121212">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<!-- jQuery library (served from Google) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<!-- bxSlider Javascript file -->
+		<script src="<?php echo get_template_directory_uri(); ?>/library/js/jquery.bxslider.js"></script>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/library/css/bxslider.css" type="text/css" media="screen" />
+
+		<?php // wordpress head functions ?>
+		<?php wp_head(); ?>
+		<?php // end of wordpress head ?>
+
+		<?php // drop Google Analytics Here ?>
+		<?php // end analytics ?>
+
+	</head>
+
+	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+
+		<div id="container">
